87c277ebc5950bd265171c306a671133a1a403fa
Vanessa rose Consigna <vconsigna.intern.integratr@outlook.com>
2023-03-31 17:06:04 +0800

    Activity2

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+
+  function checkPassword(str)
+  {
+    // at least one number, one lowercase and one uppercase letter
+    // at least six characters
+    var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    return re.test(str);
+  }
+
+</script>
